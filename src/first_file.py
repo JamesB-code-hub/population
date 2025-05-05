76c3dd6307d6f8ae99800c94239d09088840c1fc
JamesB-code-hub <jb2000uni@gmail.com>
2025-05-05 10:22:51 +0100

    Initial commit

---
+++ src/first_file.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def hello() -> str:
+    print(" nsot hello")
+    return "hello"
+
+
+x = pd.DataFrame()
+hello()
+print(x)
---
+++ tests/test_first_file.py
@@ -0,0 +1,5 @@
+from src.first_file import hello
+
+
+def test_hello():
+    assert hello() == "hello"
